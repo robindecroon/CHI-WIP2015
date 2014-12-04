27c4cb6d7fd300ac174a4b8c5a00fef3746491ea
robindecroon <robin.decroon@student.kuleuven.be>
2014-12-04 16:10:04 +0100

    forgot to actually add the script...

---
+++ data/random_parallel_data.py
@@ -0,0 +1,28 @@
+import names
+import random
+
+fout=open("testdata.csv","w")
+fout.write("name,cholesterol,inkomen,#sigaretten,alcohol,systolische bloeddruk,diastolische bloeddruk,gewicht,glycemie,leeftijd,lat,lng,geslacht,icpc,count,#dagen sinds laatste bezoek\n")
+for num in range(1,100):
+	gender = random.choice(['male', 'female'])
+	patient_name = names.get_full_name(gender=gender)
+	cholesterol = random.randint(90,210)
+	income = random.uniform(800,5000)
+	smoker = random.choice(["roker", "niet-roker"])
+	if smoker == "roker":
+		sigarets = random.randint(1, 20)
+	else:
+		sigarets = 0
+	alcohol = random.randint(0,5)
+	blood_pressure_high = random.randint(70,190)
+	blood_pressure_low = random.randint(40,100)
+	weight = random.randint(20,150)
+	sugar = random.randint(50,160)
+	age = random.randint(1,110)
+	days_since_last_visit = random.randint(1,100)
+	lat = random.uniform(50.687431, 51.049931)
+	lng = random.uniform(3.889410, 5.188080)
+	icpc = random.choice(["Aids en hiv-infectie", "Kanker", "Gezichtsstoornissen", "Gehoorstoornissen", "Aangeboren afwijkingen van het hartvaatstelsel", "Aandoeningen van het endocard/hartklepafwijkingen", "Hartfalen", "Coronaire hartziekten", "Hartritmestoornissen", "Beroerte", "Reumatoide artritis", "Perifere artrose", "Chronische nek- en rugklachten", "Osteoporose", "Ziekte van Parkinson", "Epilepsie", "Migraine", "Aandoeningen gerelateerd aan alcohol", "Dementie (inclusief alzheimer)", "Schizofrenie", "Stemmingsstoornissen", "Angststoornissen", "Overspanning/burn-out", "Persoonlijkheidsstoornissen", "Verstandelijke handicap", "COPD", "Astma", "Diabetes mellitus"])
+	printline = patient_name + "," + str(cholesterol) + "," + str(income) + "," + str(sigarets) + "," + str(alcohol) + ","+ str(blood_pressure_high) + "," + str(blood_pressure_low) + "," + str(weight) + "," +str(sugar) + "," + str(age) + "," + str(lat) + "," + str(lng) + "," +  gender + "," + icpc +",1," + str(days_since_last_visit) + '\n'
+	fout.write(printline)
+fout.close()
